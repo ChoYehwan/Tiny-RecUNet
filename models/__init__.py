35403a85a3510699d4a419de09eb4853a6a04380
YEHWAN CHO <154236661+ChoYehwan@users.noreply.github.com>
2025-11-23 23:07:23 +0900

    Add files via upload

--- models/__init__.py
+++ models/__init__.py
@@ -1,9 +1,11 @@
 from .UNet import UNet
 from .SwinUnet import SwinUnet
 from .TransUnet import TransUnet
+from .SwinU_TRM import SwinU_TRM
 
 model_dict = {
     "UNet": UNet,
     "SwinUnet": SwinUnet,
     "TransUnet": TransUnet,
+    "SwinU_TRM": SwinU_TRM,
 }
